94e54f8909153e046797a4cc8d3d97d10fc0ae58
Suhur Omar Farah <itssuhuromar@gmail.com>
2020-03-02 21:33:27 +0000

    assertEqual

---
+++ assertEqual.js
@@ -0,0 +1,13 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+    if (actual === expected){
+        console.log(`✅✅✅ Assertion Passed: 1 === 1`)
+    } else {
+        console.log("🛑🛑🛑 Assertion Failed: Lighthouse Labs !== Bootcamp");
+    }
+
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
