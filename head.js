135f1bc3b7ece83a45c7163005fce517bbba6171
Suhur Omar Farah <itssuhuromar@gmail.com>
2020-03-02 21:48:18 +0000

    Head

---
+++ head.js
@@ -0,0 +1,20 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+    if (actual === expected){
+        console.log(`These two arguemnts are the same: ${actual} vs ${expected}`)
+    } else {
+        console.log("`These two are not the same: ${actual} vs ${expected}`");
+    }
+
+};
+
+const head = function(array)
+{
+    return(array[0])
+}
+
+// TEST CODE
+//assertEqual("Lighthouse Labs", "Bootcamp");
+//assertEqual(1, 1); 
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
