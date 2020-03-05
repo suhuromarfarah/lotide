667fc887fb77fb14c0c6a21ad69c0378e10aa653
Suhur Omar Farah <itssuhuromar@gmail.com>
2020-03-05 01:11:56 +0000

    objects

---
+++ eqObjects.js
@@ -0,0 +1,56 @@
+const assertEqual = function(actual, expected) {
+    if (actual === expected){
+        console.log(`These two are the same: ${actual} vs ${expected}`)
+    } else {
+        console.log(`These two are not the same: ${actual} vs ${expected}`);
+    }
+
+};
+
+const eqArrays = function(a1, a2)
+{
+    if(a1.length === a2.length)
+    {
+        for (var i = 0; i < a1.length; i ++)
+        {
+            if(a1[i] !== a2[i])
+            {
+                return false;
+            }
+        }
+    }
+    else
+    {
+        return false;
+    }
+    return true;
+};
+
+// Returns true if both objects have identical keys with identical values.
+// Otherwise you get back a big fat false!
+const eqObjects = function(object1, object2) {
+    if (Object.keys(object1).length !== (Object.keys(object2).length)) {
+        return false
+    } else if (Object.keys(object1).length === (Object.keys(object2).length)) { 
+        for (let key of Object.keys(object1)) {
+            if (Array.isArray(object1[key]) && Array.isArray(object2[key])) {
+                if (!eqArrays(object1[key], object2[key])){
+                    return false;
+                }
+            } else {
+                if (typeof object1[key] === 'object' && typeof object2[key] === 'object') {
+                    return (eqObjects(object1[key], object2[key]));
+                }
+            }
+        }
+        return true;
+    }
+    return false;
+};
+
+
+const ab = { a: "1", b: "2" };
+const ba = { b: "2", a: "1" };
+console.log(eqObjects(ab, ba)); // => true
+
+assertEqual(eqObjects(ab, ba), true);
--- letterPositions.js
+++ letterPositions.js
@@ -1,56 +1,56 @@
-// FUNCTION IMPLEMENTATION
-const assertEqual = function(actual, expected) {
-    if (actual === expected){
-        console.log(`These two are the same: ${actual} vs ${expected}`)
-    } else {
-        console.log(`These two are not the same: ${actual} vs ${expected}`);
-    }
+// // FUNCTION IMPLEMENTATION
+// const assertEqual = function(actual, expected) {
+//     if (actual === expected){
+//         console.log(`These two are the same: ${actual} vs ${expected}`)
+//     } else {
+//         console.log(`These two are not the same: ${actual} vs ${expected}`);
+//     }
 
-};
+// };
 
-const eqArrays = function(a1, a2)
-{
-    if(a1.length === a2.length)
-    {
-        for (var i = 0; i < a1.length; i ++)
-        {
-            if(a1[i] !== a2[i])
-            {
-                return false;
-            }
-        }
-    }
-    else
-    {
-        return false;
-    }
-    return true;
-
-
-}
-const assertArraysEqual = function(a1, a2)
-{
-    if(a1.length === a2.length)
-    {
-        for (var i = 0; i < a1.length; i ++)
-        {
-            if(a1[i] !== a2[i])
-            {
-                console.log("Not the same")
-                return false;
-            }
-        }
-    }
-    else
-    {
-        console.log("Not the same because not the same length")
-        return false;
-    }
-    console.log("The same")
-    return true;
+// const eqArrays = function(a1, a2)
+// {
+//     if(a1.length === a2.length)
+//     {
+//         for (var i = 0; i < a1.length; i ++)
+//         {
+//             if(a1[i] !== a2[i])
+//             {
+//                 return false;
+//             }
+//         }
+//     }
+//     else
+//     {
+//         return false;
+//     }
+//     return true;
 
 
-}
+// }
+// const assertArraysEqual = function(a1, a2)
+// {
+//     if(a1.length === a2.length)
+//     {
+//         for (var i = 0; i < a1.length; i ++)
+//         {
+//             if(a1[i] !== a2[i])
+//             {
+//                 console.log("Not the same")
+//                 return false;
+//             }
+//         }
+//     }
+//     else
+//     {
+//         console.log("Not the same because not the same length")
+//         return false;
+//     }
+//     console.log("The same")
+//     return true;
+
+
+// }
 
 const letterPosition = function(sentence){
     const countNum = {};
@@ -64,10 +64,9 @@ const letterPosition = function(sentence){
 
     return countNum;
 }
-
-// console.log(letterPosition("lighthouse in the house"))
+console.log(letterPosition("lighthouse in the house"))
 }
 
 
 // TEST CODE
-assertArraysEqual(letterPositions("hello").e, [1]);
+// assertArraysEqual(letterPositions("hello").e, [1]);
